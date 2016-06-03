f0f113adc5e89751475057347ea98c0bbd3b2d53
Gerard Roma <g-roma@users.noreply.github.com>
2016-06-03 19:06:32 +0100

    adding hpss module

--- untwist/__init__.py
+++ untwist/__init__.py
@@ -1,3 +1,3 @@
 __version__ = '0.1.dev0'
 
-__all__ = ['base', 'data', 'soundcard', 'transforms', 'factorizations', 'neuralnetworks']
+__all__ = ['base', 'data', 'soundcard', 'transforms', 'factorizations', 'neuralnetworks', 'hpss']
