27611bac7ea393d119730c7b406b082663e6dc36
Gerard Roma <g.roma@surrey.ac.uk>
2016-08-02 20:35:01 +0100

    modify test for sgd with learning rate decay

--- untwist/neuralnetworks/tests/test_mlp.py
+++ untwist/neuralnetworks/tests/test_mlp.py
@@ -11,15 +11,17 @@ def test_mlp():
     dist = np.random.multivariate_normal
     x1 = normal(mean1, cov, 2000)
     x2 = normal(mean2, cov, 2000)
-    y1 = np.ones((2000,1))
-    y2 = np.zeros((2000,1))
+    y1 = np.ones((1000,1))
+    y2 = np.zeros((1000,1))
     ds = Dataset(2, float, 1, int)
-    ds.add(x1[:1000,:],y1[:1000])
-    ds.add(x2[:1000,:],y2[:1000])
+    ds.add(x1[:1000,:],y1)
+    ds.add(x2[:1000,:],y2)
     mlp = MLP(2, 1, [2])
-    sgd = SGD(mlp, iterations = 1000)
+    sgd = SGD(mlp, rate_decay_th=0,iterations = 1000)
     sgd.train(ds)
     y = sgd.predict(x1[1000:,:])
+    print np.sum(y > 0.5) , y.shape[0]
     assert(np.sum(y > 0.5) == y.shape[0])
     y = sgd.predict(x2[1000:,:])
+    print np.sum(y < 0.5), y.shape[0]
     assert(np.sum(y < 0.5) == y.shape[0])
