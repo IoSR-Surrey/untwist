7a5e3ff4a386231ffb7ab70d8345f5db2714d3a9
Gerard Roma <g-roma@users.noreply.github.com>
2016-08-11 11:30:38 -0400

    introducing implicit parallel processing for real

---
+++ untwist/base/parallel.py
@@ -0,0 +1,70 @@
+"""
+Multiprocessing decorator
+"""
+import multiprocessing as mp
+import numpy as np
+import copy_reg
+import imp, sys
+f, pathname, desc = imp.find_module("types", sys.path[1:])
+module = imp.load_module("systypes", f, pathname, desc)
+from systypes import MethodType
+
+n_threads = 4
+
+# allow pickling instance methods
+# https://gist.github.com/bnyeggen/1086393
+from multiprocessing import Pool
+from functools import partial
+
+def _pickle_method(method):
+	func_name = method.im_func.__name__
+	obj = method.im_self
+	cls = method.im_class
+	if func_name.startswith('__') and not func_name.endswith('__'): 
+		cls_name = cls.__name__.lstrip('_')
+		func_name = '_' + cls_name + func_name
+	return _unpickle_method, (func_name, obj, cls)
+
+def _unpickle_method(func_name, obj, cls):
+	for cls in cls.__mro__:
+		try:
+			func = cls.__dict__[func_name]
+		except KeyError:
+			pass
+		else:
+			break
+	return func.__get__(obj, cls)
+
+
+copy_reg.pickle(MethodType, _pickle_method, _unpickle_method)
+
+    
+class parallel_process(object):
+    def __init__(self, input_dim, output_dim):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        
+    def worker(self, a):
+        return a[0].process(*a[1:])
+
+    def __call__(self, process_func):
+
+        def wrapper(*args):
+            data_obj = args[1]
+            if (len(data_obj.shape) <= self.input_dim 
+                or data_obj.shape[-1] == 1):
+                return process_func(*args)
+            else:
+                pool = mp.Pool(mp.cpu_count())# TODO: make configurable
+                arglist = [
+                    (args[0],) + 
+                    (data_obj[...,i],) + 
+                    args[2:]
+                    for i in range(data_obj.shape[-1])
+                ]
+                result = pool.map(self.worker, arglist)
+                if self.output_dim > self.input_dim: #expanding
+                    return np.stack(result, -1)
+                else: #contracting
+                    return np.concatenate(result, -1)
+        return wrapper
