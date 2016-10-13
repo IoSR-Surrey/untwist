fix normalization in istft overlap-add

untwist/transforms/stft.py
@@ -15,24 +15,24 @@ class STFT(Processor):
     Input should be a mono Wave, output is a complex spectrogram
     """
 
-    def __init__(self, window=None, fft_size=1024, hop_size=512):
+    def __init__(self, window = None, fft_size = 1024, hop_size = 512):
         if window is None:
             self.window = np.hanning(fft_size)
         else:
             self.window = window
         self.fft_size = fft_size
-        self.hop_size = hop_size        
+        self.hop_size = hop_size
+        self.window_size = len(self.window)
+        self.half_window = int(np.floor(len(self.window) / 2.0))
      
     @parallel_process(1,2)
     def process(self, wave):
         wave.check_mono()
-        window_size = len(self.window)
-        half_window = int(np.floor(window_size / 2.0))
-        wave = wave.zero_pad(half_window, half_window)        
-        num_frames = int(1 + np.ceil((wave.shape[0] - window_size) / 
+        wave = wave.zero_pad(self.half_window, self.half_window)
+        num_frames = int(1 + np.ceil((wave.shape[0] - self.window_size) /
             float(self.hop_size)))                
         col_size = wave.strides[0]
-        frames = stride_tricks.as_strided(wave, shape=(num_frames, window_size),
+        frames = stride_tricks.as_strided(wave, shape=(num_frames, self.window_size),
             strides=(col_size*self.hop_size, col_size)).copy()
         frames *= self.window
         transform = np.fft.rfft(frames, self.fft_size)
@@ -46,7 +46,7 @@ class ISTFT(Processor):
     Input should be a complex spectrogram, output is mono Wave
     """   
 
-    def __init__(self, window=None, fft_size=1024, hop_size=512, sample_rate = 44100):
+    def __init__(self, window = None, fft_size = 1024, hop_size = 512, sample_rate = 44100):
         if window is None:
             self.window = np.hanning(fft_size)
         else:
@@ -54,14 +54,22 @@ class ISTFT(Processor):
         self.fft_size = fft_size
         self.hop_size = hop_size
         self.sample_rate = sample_rate
+        self.window_size = len(self.window)
+        self.half_window = int(np.floor(len(self.window) / 2.0))
         
     @parallel_process(2,1)
     def process(self, spectrogram):
         frames = np.fft.irfft(spectrogram.T)
-        result_length = ((frames.shape[0] - 1) * self.hop_size) + self.fft_size
+        n_frames = frames.shape[0]
+        result_length = ((n_frames - 1) * self.hop_size) + self.fft_size
         result = np.zeros((result_length, 1))
-        for i in range(frames.shape[0]):
+        norm = np.zeros((result_length, 1))
+        wsquare = (self.window * self.window).reshape(-1, 1)
+        for i in range(n_frames):
             indices = i * self.hop_size + np.r_[0:self.fft_size]
-            result[indices] = result[indices] + frames[i,:].reshape(self.fft_size,1)
-        result = result[self.hop_size:result_length - self.hop_size]
+            result[indices] +=  frames[i,:].reshape(-1, 1) * self.window.reshape(-1, 1)
+            norm[indices] += wsquare
+        min_float = np.finfo(spectrogram.dtype).tiny
+        result /= np.where(norm > min_float, norm, 1.0)
+        result = result[self.half_window:result_length - self.half_window]
         return audio.Wave(result, self.sample_rate)

untwist/transforms/tests/test_stft.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 from ...data import Wave
-from ...transforms.stft import STFT, ISTFT
+from ...transforms import STFT, ISTFT
 
 def test_stft():
     audio_dir = os.path.dirname(__file__) + "/" + ("../") * 3 + "audio/"
@@ -9,4 +9,5 @@ def test_stft():
     samples = sine1.shape[0]
     spectrogram = STFT().process(sine1)
     sine2 = ISTFT().process(spectrogram)
-    assert(np.sum(sine1 - sine2[:samples,:]) < 0.03)
+    assert(np.sum(np.abs(sine1 - sine2[:samples,:])) < 1e-10)
+