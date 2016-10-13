e155f783b14984352d776c85034cb3bb30f63987
Gerard Roma <g-roma@users.noreply.github.com>
2016-10-13 19:52:28 +0100

    easier mlp test

--- untwist/neuralnetworks/tests/test_mlp.py
+++ untwist/neuralnetworks/tests/test_mlp.py
@@ -8,8 +8,8 @@ from numpy.random import multivariate_normal as normal
 def test_mlp():
     np.random.seed(0)
     mean1 = [0, 0]
-    mean2 = [20, 20]
-    cov = [[2, 0], [0, 2]]    
+    mean2 = [30, 30]
+    cov = [[2, 0], [0, 2]]
     x1 = normal(mean1, cov, 2000)
     x2 = normal(mean2, cov, 2000)
     y1 = np.ones((1000,1))
@@ -17,7 +17,7 @@ def test_mlp():
     ds = Dataset(2, float, 1, int)
     ds.add(x1[:1000,:],y1)
     ds.add(x2[:1000,:],y2)
-    mlp = MLP(2, 1, [2])
+    mlp = MLP(2, 1, [2,2])
     sgd = SGD(mlp, rate_decay_th=0,iterations = 1000)
     sgd.train(ds)
     y = sgd.predict(x1[1000:,:])
