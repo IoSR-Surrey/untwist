274a13f4ab026f5ad8553d1866e3d745856380e5
Gerard Roma <g-roma@users.noreply.github.com>
2017-05-03 23:41:09 -0400

    change rpca orientation

--- untwist/factorizations/rpca.py
+++ untwist/factorizations/rpca.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import numpy as np
-from numpy import linalg
+from numpy.linalg import svd, norm
+from scipy.sparse.linalg import svds
 from ..base.algorithms import Processor
 
 class RPCA(Processor):
@@ -8,6 +9,19 @@ class RPCA(Processor):
     Robust PCA, Inexact ALM method
     Based on http://perception.csl.illinois.edu/matrix-rank/sample_code.html
     """
+
+
+    def choosvd(self, n, d):
+        y = False
+        if n   <= 100: y = d / n <= 0.02
+        elif n <= 200: y = d/n <= 0.06
+        elif n <= 300: y = d/n <=0.26
+        elif n <= 400: y = d/n <=0.28
+        elif n <= 500: y = d/n <=0.34
+        else: y = d/n <=0.38
+        return y 
+    
+
 
     def __init__(self, iterations = 100, threshold = None, l = 1, mu = 1.25, rho = 1.5):
         self.iterations = iterations
@@ -17,38 +31,47 @@ class RPCA(Processor):
         self.rho = rho
 
     def process(self, X):
-        Y = X
-        n = Y.shape[0]
-        m = Y.shape[1]
+        X = X.T
+        Y = X.copy()
+        (m, n) = Y.shape
+        n = Y.shape[1]
         self.l = self.l / np.sqrt(m)
-        norm_two = linalg.norm(Y.ravel(), 2)
-        norm_inf = linalg.norm(Y.ravel(), np.inf) / self.l
+        u,s,v = svds(Y,1,which="LM")
+        norm_two = s[0]
+        norm_inf = norm(Y.ravel(), np.inf) / self.l
         dual_norm = np.max([norm_two, norm_inf])
         Y = Y / dual_norm
         A = np.zeros(Y.shape)
         E = np.zeros(Y.shape)
-        d_norm = linalg.norm(X, 'fro')
+        d_norm = norm(X, 'fro')
         mu = self.mu / norm_two
         mu_bar = mu * 1e7
-        sv = 10.0
+        sv = 10
 
 
         for i in range(self.iterations):
             temp_T = X - A + (1 / mu) * Y
             E = np.maximum(temp_T - self.l / mu, 0)
             E += np.minimum(temp_T + self.l / mu, 0)
-            U, S, V = linalg.svd(X - E + (1 / mu) * Y, full_matrices = False)
-            svp = (S > 1 / mu).shape[0]
-            if svp < sv:
-                sv = np.min([svp + 1, n])
+            sparse_svd = self.choosvd( n, sv)
+            if sparse_svd:
+                U, S, V = svds(X - E + (1 / mu) * Y, sv, which= "LM")                
             else:
-                sv = np.min([svp + round(0.05 * n), n])
-            A = np.dot(np.dot(U[:, :svp], np.diag(S[:svp] - 1 / mu)), V[:svp, :])
+                U, S, V = svd(X - E + (1 / mu) * Y, full_matrices = False)
+            svp = len(np.where(S > (1 / mu))[0])
+            if svp < sv:
+                sv = int(np.min([svp+1, n]))
+            else:
+                sv = int(np.min([svp + np.round(0.05 * n), n]))
+            if sparse_svd:
+                A = np.dot(np.dot(U[:,-svp:], np.diag(S[-svp:] -1/mu)), V[-svp:,:])
+            else:
+                A = np.dot(np.dot(U[:, :svp], np.diag(S[:svp] - 1 / mu)), V[:svp, :])
             Z = X - A - E
             Y = Y + mu * Z
             mu = np.min([mu * self.rho, mu_bar])
-            err = linalg.norm(Z, 'fro') / d_norm
+            err = norm(Z, 'fro') / d_norm
             print(i, err)
             if self.threshold is not None and err < self.threshold:
                 break
-        return A, E
+        return A.T, E.T
