905b60b6017cf75be0b4599bb70d90f0e022ad4b
deeuu <deeuu@users.noreply.github.com>
2017-11-01 11:31:20 +0000

    Fixed MLP example. Script for using DSD100 stems

--- examples/mlp_example.py
+++ examples/mlp_example.py
@@ -1,51 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import theano
-from untwist.data import Wave, Dataset, BinaryMask
-from untwist.transforms import STFT, ISTFT
-from untwist.neuralnetworks import MLP, SGD
+from untwist import data
+from untwist import transforms
+from untwist import neuralnetworks
+from utils import get_stems
 floatX = theano.config.floatX
 
-n_bins = 513
-train_frames = 10000
+# Load the stems
+stems = get_stems(song_idx=0,
+                  path_to_dsd100_subset='/scratch/DSD100subset',
+                  mono=True)
 
-target = Wave.read("target.wav")[:, 0]
-background = Wave.read("background.wav")[:, 0]
-mix = target + background
+train_frames = 2000
+fft_size = 1024
+n_bins = fft_size // 2 + 1
 
-stft = STFT()
-istft = ISTFT()
-mlp = MLP(n_bins, n_bins, [n_bins, n_bins])
-sgd = SGD(mlp,
-          learning_rate=0.05, momentum=0.2,
-          batch_size=200, iterations=100)
+stft = transforms.STFT(fft_size=fft_size)
+istft = transforms.ISTFT(fft_size=fft_size)
 
-X = stft.process(mix)
-T = stft.process(target)
-B = stft.process(background)
+# Compute spectrograms of the mixture, vocals and accompaniment
+mix_spec = stft.process(stems.mixture)
+vocals_spec = stft.process(stems.vocals)
+accompaniment_spec = stft.process(stems.accompaniment)
 
-ideal_mask = BinaryMask(T.magnitude(), B.magnitude())
-ds = Dataset(n_bins, floatX, n_bins, np.bool_)
-Xtrain = X[:, :train_frames].magnitude().T
+# The network and training algorithm
+mlp = neuralnetworks.MLP(input_size=n_bins,
+                         output_size=n_bins,
+                         hidden_sizes=[n_bins, n_bins])
+
+sgd = neuralnetworks.SGD(mlp,
+                         learning_rate=0.05,
+                         momentum=0.2,
+                         batch_size=200,
+                         iterations=100)
+
+# Ideal mask for the vocals
+ideal_mask = data.audio.BinaryMask(
+    vocals_spec.magnitude(), accompaniment_spec.magnitude())
+
+# Create dataset for adding training data
+ds = data.dataset.Dataset(x_width=n_bins,
+                          x_type=floatX,
+                          y_width=n_bins,
+                          y_type=np.bool_)
+
+'''
+Mixture spectrogram as input, ideal mask for vocal as output
+Need to transpose so each column is a frequency bin
+'''
+Xtrain = mix_spec[:, :train_frames].magnitude().T
 Ytrain = ideal_mask[:, :train_frames].T
-print(Xtrain.shape, Ytrain.shape, n_bins)
 
+
+# Add to the dataset and standardise
 ds.add(Xtrain, Ytrain)
 ds.standardize()
 ds.shuffle()
+
+# Go on then...
 sgd.train(ds)
 
-Xtest = X[:, train_frames:].magnitude().T
-Xtest = ds.standardize_points(Xtest)
-pred = sgd.predict(Xtest)
-estimated_mask = np.empty_like(Xtest.T)
-estimated_mask[:] = pred.T > 0.5
+# Predict using remaining spectra
+test_data = mix_spec[:, train_frames:].magnitude().T
+test_data = ds.standardize_points(test_data)
+test_prediction = sgd.predict(test_data)
+
+# Predict the mask, threshold to get binary (> 0.5)
+estimated_mask = ideal_mask[:, train_frames:].copy()
+estimated_mask[:] = test_prediction.T > 0.5
+
+# Example audio
+ideal_vocal = mix_spec[:, train_frames:] * ideal_mask[:, train_frames:]
+istft.process(ideal_vocal).write('ideal_masked_vocal.wav')
+predicted_vocal = mix_spec[:, train_frames:] * estimated_mask
+istft.process(predicted_vocal).write('predicted_vocal.wav')
+
+# Transpose back to bin x time and plot
 plt.subplot(4, 1, 1)
-Xtest.T.plot(label_x=False, title="mixture")
+test_data.T.plot(title="mixture", xlabel=False)
 plt.subplot(4, 1, 2)
-T[:, train_frames:].plot(label_x=False, title="target signal")
+vocals_spec[:, train_frames:].plot(title="target signal", xlabel=False)
 plt.subplot(4, 1, 3)
-ideal_mask[:, train_frames:].plot(label_x=False, title="ideal mask")
+ideal_mask[:, train_frames:].plot(title="ideal mask", xlabel=False)
 plt.subplot(4, 1, 4)
 estimated_mask.plot(title="estimated mask")
 plt.show()
---
+++ examples/utils.py
@@ -0,0 +1,56 @@
+from untwist import data
+from collections import namedtuple
+import os
+
+
+Stems = namedtuple('Stems', 'drums bass other vocals accompaniment mixture')
+
+
+def get_stems(song_idx=0,
+              path_to_dsd100_subset='/scratch/DSD100subset',
+              mono=False):
+    '''
+    Load drums, bass, 'other', vocals, accompaniment and the mixture from four
+    songs of the Demixing Secret Database 100 (DSD100).
+
+    song_idx: int (0, 1, 2, or 3)
+              index of the song to load
+    path_to_dsd100_subset: string
+              path to https://www.loria.fr/~aliutkus/DSD100subset.zip
+    mono: bool
+          Converts wavs to mono if true.
+    '''
+
+    if song_idx == 0:
+        path = "Sources/Test/005 - Angela Thomas Wade - Milk Cow Blues"
+    elif song_idx == 1:
+        path = "Sources/Test/049 - Young Griffo - Facade"
+    elif song_idx == 2:
+        path = "Sources/Dev/055 - Angels In Amplifiers - I'm Alright"
+    elif song_idx == 3:
+        path = "Sources/Dev/081 - Patrick Talbot - Set Me Free"
+
+    path = os.path.join(path_to_dsd100_subset,
+                        path)
+
+    if mono:
+        def _func(x):
+            return x.as_mono()
+    else:
+        def _func(x):
+            return x
+
+    stems = [_func(data.audio.Wave.read(os.path.join(path, _ + '.wav')))
+             for _ in ['drums', 'bass', 'other', 'vocals']
+             ]
+
+    stems = Stems(
+        drums=stems[0],
+        bass=stems[1],
+        other=stems[2],
+        vocals=stems[3],
+        accompaniment=sum(stems[:3]),
+        mixture=sum(stems),
+    )
+
+    return stems
