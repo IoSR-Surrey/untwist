6bf52ed23a7080f902c0a8fd59c64f44e1aab372
deeuu <deeuu@users.noreply.github.com>
2017-11-02 14:41:36 +0000

    Package imports not needed

--- untwist/__init__.py
+++ untwist/__init__.py
@@ -1,11 +1,1 @@
 __version__ = '0.1.dev0'
-
-from . import (data,
-               analysis,
-               factorizations,
-               filters,
-               hpss,
-               neuralnetworks,
-               transforms,
-               utilities,
-               )
